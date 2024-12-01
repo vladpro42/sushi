60bc3bdd0f8c3d8cd4b492a588bd890950ef1945
bondarev vlad <bondarev.vlad.01@gmail.com>
2024-12-01 21:13:13 +0300

    feat: add header

--- global.js
+++ global.js
@@ -1,40 +1,40 @@
-const nav = [
-    { id: 1, name: 'Главная', href: '/index.html' },
-    { id: 2, name: 'Категории', href: '/pages/category/index.html' },
-    { id: 3, name: 'Доставка', href: '/pages/delivery/index.html' },
-]
-function initHeader(nav) {
-    const body = document.body;
-    const header = `
-    <header class="header">
-        <div class="container">
-            <div class="header__inner">
-                <nav class="nav">
-                    <ul class="nav__list"></ul>
-                </nav>
-            </div>
-        </div>
-    </header>`;
-    body.insertAdjacentHTML("beforebegin", header)
-    const navList = document.querySelector('.nav__list');
-    if(!navList) throw new Error(' не найден элемент с классом .nav__list')
-    nav.forEach(item => {
-        const li = getItem(item)
-        navList.appendChild(li);
-    })
+// const nav = [
+//     { id: 1, name: 'Главная', href: '/index.html' },
+//     { id: 2, name: 'Категории', href: '/pages/category/index.html' },
+//     { id: 3, name: 'Доставка', href: '/pages/delivery/index.html' },
+// ]
+// function initHeader(nav) {
+//     const body = document.body;
+//     const header = `
+//     <header class="header">
+//         <div class="container">
+//             <div class="header__inner">
+//                 <nav class="nav">
+//                     <ul class="nav__list"></ul>
+//                 </nav>
+//             </div>
+//         </div>
+//     </header>`;
+//     body.insertAdjacentHTML("beforebegin", header)
+//     const navList = document.querySelector('.nav__list');
+//     if(!navList) throw new Error(' не найден элемент с классом .nav__list')
+//     nav.forEach(item => {
+//         const li = getItem(item)
+//         navList.appendChild(li);
+//     })
 
-    function getItem(item) {
-        const li = document.createElement('li');
-        li.classList.add('nav__item');
+//     function getItem(item) {
+//         const li = document.createElement('li');
+//         li.classList.add('nav__item');
 
-        const a = document.createElement('a');
-        a.classList.add('nav__link');
-        a.href = item.href;
-        a.textContent = item.name;
+//         const a = document.createElement('a');
+//         a.classList.add('nav__link');
+//         a.href = item.href;
+//         a.textContent = item.name;
 
-        li.appendChild(a);
-        return li
-    }
-}
+//         li.appendChild(a);
+//         return li
+//     }
+// }
 
-initHeader(nav)
+// initHeader(nav)
