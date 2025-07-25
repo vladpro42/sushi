4636fd9f7d749f7310d8837221ff658d5d150147
vladpro42 <bondarev.vlad.01@gmail.com>
2025-07-25 15:04:22 +0300

    refactor

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,26 +1,24 @@
-new Swiper('.top__swiper', {
-    direction: 'horizontal',
-    loop: true,
+new Swiper(".top__swiper", {
+  direction: "horizontal",
+  loop: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
-new Swiper('.promotion-swiper', {
-    direction: 'horizontal',
-    loop: true,
+new Swiper(".promotion-swiper", {
+  direction: "horizontal",
+  loop: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-    navigation: {
-        nextEl: '.promotion-swiper .swiper-button-next',
-        prevEl: '.promotion-swiper .swiper-button-prev',
-      },
-
+  navigation: {
+    nextEl: ".promotion-swiper .swiper-button-next",
+    prevEl: ".promotion-swiper .swiper-button-prev",
+  },
 });
