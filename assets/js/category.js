65e4a44256e37e75eb5ca35f20466b432f3e50d9
vladpro42 <bondarev.vlad.01@gmail.com>
2026-01-07 21:21:09 +0300

    feat: добавил изменения

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,18 +1,38 @@
 const btn = document.querySelector(".food__filter-btn");
 if (btn) {
   btn.addEventListener("click", (event) => {
-    const ingredients = document.querySelector(".food__ingredients");
-    if (!ingredients) return;
+    if (window.innerWidth > 1023) {
+      const ingredients = document.querySelector(".food__ingredients");
+      if (!ingredients) return;
 
-    ingredients.classList.add("open");
+      ingredients.classList.add("open");
 
-    gsap.to(ingredients, {
-      duration: 0.45,
-      opacity: 1,
-      y: 15,
-      ease: "power3.out",
-    });
+      gsap.to(ingredients, {
+        duration: 0.45,
+        opacity: 1,
+        y: 15,
+        ease: "power3.out",
+      });
+    } else {
+      const filterMenuMobile = document.querySelector('.filters-menu__mobile')
+      if (filterMenuMobile) {
+        filterMenuMobile.classList.add('filters-menu__mobile--active')
+      }
+    }
+
   });
+}
+
+// filters-menu__close btn-close
+
+const btnCloseFilterMenuClose = document.querySelector('.filters-menu__close')
+if (btnCloseFilterMenuClose) {
+  btnCloseFilterMenuClose.addEventListener('click', event => {
+    const filterMenuMobile = document.querySelector('.filters-menu__mobile')
+    if (filterMenuMobile) {
+      filterMenuMobile.classList.remove('filters-menu__mobile--active')
+    }
+  })
 }
 
 const closeBtn = document.querySelector(".food__ingredients-btn");
@@ -48,16 +68,16 @@ foodItems.forEach((item, index) => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll(".food__select-box").forEach(filterBtn => {
-        filterBtn.addEventListener('click', event => {
-            const target = event.currentTarget;
-            if(target.classList.contains("active")) {
-                target.classList.remove("active");
-                target.classList.add("hide");
-                return
-            }
-            target.classList.add("active");
-            target.classList.remove("hide");
-        })
+  document.querySelectorAll(".food__select-box").forEach(filterBtn => {
+    filterBtn.addEventListener('click', event => {
+      const target = event.currentTarget;
+      if (target.classList.contains("active")) {
+        target.classList.remove("active");
+        target.classList.add("hide");
+        return
+      }
+      target.classList.add("active");
+      target.classList.remove("hide");
     })
+  })
 })
